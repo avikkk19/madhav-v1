d06b2aadeaed134d9928699bc20f898bfc7d9523
avikkk19 <avinashkamadri14@gmail.com>
2025-04-06 18:56:07 +0530

    no fixes refined ui

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "./dist", // Ensure Vercel finds the build output
+  },
+});
