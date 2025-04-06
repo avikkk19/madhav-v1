fa041b8269451841f62d9522d82c04a32fc83e24
avikkk19 <avinashkamadri14@gmail.com>
2025-04-06 18:54:04 +0530

    vercel deployed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { supabase } from "./components/SupabaseClient.jsx";
-import SignIn from "./components/SignIn.jsx";
+import SignIn from "./components/Signin.jsx";
 import SignUp from "./components/SignUp.jsx";
 import HeroSection from "./components/HeroSection.jsx";
 
